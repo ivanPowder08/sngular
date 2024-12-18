e0639691d808c36b410fb241a4635d040d4ae0be
Ivan Martinez Rodriguez <imartinezr@outlook.com>
2024-12-18 10:24:28 -0600

    Mi primer App Vue3

---
+++ miprimerapp/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import micomponente from './components/micomponente.vue'
+</script>
+
+<template>
+  <main>
+    <micomponente />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
